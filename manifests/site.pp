81514c7513d67cb8f90fe4d7c7fb3a2ae429385e
Graham Taylor <graham.x.taylor@capgemini.com>
2014-12-04 15:19:07 +0000

    Add mesosphere repository

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,26 @@
 
 Exec { path => ["/usr/bin", "/usr/sbin", "/bin"] }
 
+exec { 'install-mesos-repo':
+  command   => 'rpm -Uvh http://repos.mesosphere.io/el/6/noarch/RPMS/mesosphere-el-repo-6-2.noarch.rpm',
+  creates   => '/etc/yum.repos.d/mesosphere.repo',
+  logoutput => on_failure
+}
+
 service { 'iptables':
-  ensure   => 'stopped',
-  enable => 'false',
+  ensure  => 'stopped',
+  enable  => 'false',
 }
 
 include docker
 include zookeeper
 #include marathon
 
-class{'mesos::master': }
+class{ 'mesos::master':
+  require => Exec['install-mesos-repo']
+}
 
 class{ 'mesos::slave':
-  zookeeper  => 'zk://127.0.0.1:2181'
+  zookeeper  => 'zk://127.0.0.1:2181',
+  require    => Exec['install-mesos-repo']
 }
