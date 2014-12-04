9a0c09c1c8beef11e66d5e3d4c25b331c050bd4a
Graham Taylor <graham.x.taylor@capgemini.com>
2014-12-04 17:32:46 +0000

    Moving to ubuntu

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,26 +1,21 @@
+augeas {
+  'selinux':
+  context => '/files/etc/sysconfig/selinux',
+  changes => [ 'set SELINUX disabled' ];
+}
 
 Exec { path => ["/usr/bin", "/usr/sbin", "/bin"] }
 
-exec { 'install-mesos-repo':
-  command   => 'rpm -Uvh http://repos.mesosphere.io/el/6/noarch/RPMS/mesosphere-el-repo-6-2.noarch.rpm',
-  creates   => '/etc/yum.repos.d/mesosphere.repo',
-  logoutput => on_failure
-}
-
-service { 'iptables':
-  ensure  => 'stopped',
-  enable  => 'false',
-}
-
+include java
 include docker
-include zookeeper
-#include marathon
 
-class{ 'mesos::master':
-  require => Exec['install-mesos-repo']
-}
+#class { 'zookeeper':
+#  require => Exec['install-zookeeper-repo']
+#}
+#include zookeeper
+include mesos::master
+include marathon
 
 class{ 'mesos::slave':
-  zookeeper  => 'zk://127.0.0.1:2181',
-  require    => Exec['install-mesos-repo']
+  zookeeper  => 'zk://127.0.0.1:2181'
 }
