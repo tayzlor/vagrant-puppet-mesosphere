74af521d4ed7a34504a761a4904da002d5e77cd8
Cam Parry <cam.parry@gmail.com>
2014-12-12 13:49:13 +0000

    Centos fixes

--- modules/basenode/manifests/init.pp
+++ modules/basenode/manifests/init.pp
@@ -5,5 +5,24 @@ class basenode() {
     ensure => present
   }
 
+  case $::osfamily {
+    'Redhat': {
+
+      file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-mesosphere':
+        ensure => present,
+        source => 'puppet:///modules/basenode/RPM-GPG-KEY-mesosphere'
+      }
+
+      yumrepo { "mesosphere":
+        baseurl  => "http://repos.mesosphere.io/el/${::operatingsystemmajrelease}/",
+        descr    => "Mesosphere Packages for EL - ${::operatingsystemmajrelease}",
+        enabled  => 1,
+        gpgcheck => 1,
+        gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mesosphere',
+        require  => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-mesosphere'],
+      }
+    }
+  }
+
   include docker
 }
