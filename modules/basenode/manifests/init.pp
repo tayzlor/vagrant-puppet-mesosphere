726361e0c39208a0096c691e958d1faa881d4316
Cam Parry <cam.parry@gmail.com>
2014-12-15 12:53:38 +0000

    centos changes

--- modules/basenode/manifests/init.pp
+++ modules/basenode/manifests/init.pp
@@ -1,20 +1,34 @@
 class basenode() {
   Exec { path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"] }
 
-  package { ['python-setuptools', 'python-protobuf']:
-    ensure => present
-  }
-
   case $::osfamily {
+    'Debian': {
+      package { ['python-setuptools', 'python-protobuf']:
+        ensure => present
+      }
+    }
+
     'Redhat': {
+      package { [ 'glibc-headers', 'gcc-c++', 'python-setuptools', 'protobuf']:
+        ensure => present
+      }
+
+      package { 'cloudera-cdh':
+        ensure => 'installed',
+        source => 'http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm',
+        provider => 'rpm'
+      }
 
       file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-mesosphere':
         ensure => present,
         source => 'puppet:///modules/basenode/RPM-GPG-KEY-mesosphere'
       }
 
+      exec { 'rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera':
+      }
+
       yumrepo { "mesosphere":
-        baseurl  => "http://repos.mesosphere.io/el/${::operatingsystemmajrelease}/",
+        baseurl  => "http://repos.mesosphere.io/el/${::operatingsystemmajrelease}/\$basearch/",
         descr    => "Mesosphere Packages for EL - ${::operatingsystemmajrelease}",
         enabled  => 1,
         gpgcheck => 1,
