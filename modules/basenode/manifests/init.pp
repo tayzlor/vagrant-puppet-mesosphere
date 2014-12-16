bf8ae89973e2666057ed2ae5d8f7503b37e54c27
Cam Parry <cam.parry@gmail.com>
2014-12-16 11:50:02 +0000

    Add services

--- modules/basenode/manifests/init.pp
+++ modules/basenode/manifests/init.pp
@@ -9,7 +9,7 @@ class basenode() {
     }
 
     'Redhat': {
-      package { [ 'glibc-headers', 'gcc-c++', 'python-setuptools', 'protobuf']:
+      package { [ 'glibc-headers', 'gcc-c++', 'python-setuptools', 'protobuf', 'redhat-lsb']:
         ensure => present
       }
 
@@ -19,9 +19,31 @@ class basenode() {
         provider => 'rpm'
       }
 
-      file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-mesosphere':
-        ensure => present,
-        source => 'puppet:///modules/basenode/RPM-GPG-KEY-mesosphere'
+      file { 
+        '/etc/pki/rpm-gpg/RPM-GPG-KEY-mesosphere':
+          ensure => present,
+          source => 'puppet:///modules/basenode/RPM-GPG-KEY-mesosphere';
+        '/etc/init.d/marathon':
+          ensure => present,
+          source => 'puppet:///modules/basenode/marathon',
+          mode   => '0554';
+        '/etc/init.d/zookeeper':
+          ensure => present,
+          source => 'puppet:///modules/basenode/zookeeper',
+          mode   => '0554';
+        '/etc/init.d/mesos-master':
+          ensure => present,
+          source => 'puppet:///modules/basenode/mesos-master',
+          mode   => '0554';
+        '/etc/init.d/mesos-slave':
+          ensure => present,
+          source => 'puppet:///modules/basenode/mesos-slave',
+          mode   => '0554';
+        '/opt/marathon':
+          ensure => "directory";
+        '/var/log/mesos':
+          ensure => present,
+          owner  => vagrant;
       }
 
       exec { 'rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera':
