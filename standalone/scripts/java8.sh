cfe238526755a3d3fc630dd8445559151c7d3a59
Matthias Laug <matthias.laug@takeaway.com>
2016-01-21 06:44:10 +0100

    added java8 installer script

---
+++ standalone/scripts/java8.sh
@@ -0,0 +1,12 @@
+
+
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+  sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+  sudo debconf-set-selections
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+apt-get update
+apt-get install -y oracle-java8-installer
